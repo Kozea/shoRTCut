54db7d62a568e15720b8c9e1d88c8816497c5f01
Florian Mounier <florian.mounier@kozea.fr>
2014-01-14 15:09:40 +0100

    Working on lan

--- app/routes.py
+++ app/routes.py
@@ -19,15 +19,37 @@ class WebSocket(Route, tornado.websocket.WebSocketHandler):
         log.info('Websocket opened')
 
     def on_message(self, message):
-        if message == 'caller':
-            self.caller = self
-        elif message == 'callee':
-            self.callee = self
-        elif message == 'status':
-            self.write_message('ECHO!Status: caller: %r callee: %r' % (
-                self.caller, self.callee))
+        log.debug('Websocket %s' % message)
+        if '|' in message:
+            pipe = message.index('|')
+            cmd, data = message[:pipe], message[pipe + 1:]
+        else:
+            cmd, data = message, ''
+
+        if cmd == 'IAM':
+            if data == 'caller':
+                WebSocket.caller = self
+            elif  data == 'callee':
+                WebSocket.callee = self
+            if WebSocket.callee and WebSocket.caller:
+                WebSocket.caller.write_message('GO')
+
+        elif cmd == 'STATUS':
+            self.write_message('ECHO|Status: caller: %r callee: %r' % (
+                WebSocket.caller, WebSocket.callee))
+        else:
+            if WebSocket.caller == self:
+                log.info('Caller -> Callee : %s' % message)
+                WebSocket.callee and WebSocket.callee.write_message(message)
+            if WebSocket.callee == self:
+                log.info('Callee <- Callee : %s' % message)
+                WebSocket.caller and WebSocket.caller.write_message(message)
 
     def on_close(self):
         log.info('Websocket closed')
+        if self == WebSocket.caller:
+            WebSocket.caller = None
+        if self == WebSocket.callee:
+            WebSocket.callee = None
 
 
