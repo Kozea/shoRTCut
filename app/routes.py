32017b9bdb27b638aa3f12daa80364bfa34bbf09
Florian Mounier <florian.mounier@kozea.fr>
2014-01-14 13:43:29 +0100

    Start webrtc

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,33 @@
-from . import app
-from flask import render_template
+import tornado.websocket
+from app import url, Route
+from logging import getLogger
+
+log = getLogger('apparatus')
+
+@url(r'/(caller|callee)?')
+class Index(Route):
+    def get(self, type):
+        return self.render('index.html')
 
 
-@app.route("/")
-def index():
-    return render_template('index.jinja2')
+@url(r'/ws')
+class WebSocket(Route, tornado.websocket.WebSocketHandler):
+    caller = None
+    callee = None
+
+    def open(self):
+        log.info('Websocket opened')
+
+    def on_message(self, message):
+        if message == 'caller':
+            self.caller = self
+        elif message == 'callee':
+            self.callee = self
+        elif message == 'status':
+            self.write_message('ECHO!Status: caller: %r callee: %r' % (
+                self.caller, self.callee))
+
+    def on_close(self):
+        log.info('Websocket closed')
+
+
